563b07066a9f5b8a4b6b290604321cb9ee3b5cdd
Ether Eal <jaredchristiancedilla1@gmail.com>
2025-03-06 13:08:13 +0800

    Initial Log In Form

--- hrs/hrs/Form1.Designer.vb
+++ hrs/hrs/Form1.Designer.vb
@@ -22,10 +22,131 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.BtnRegister = New System.Windows.Forms.Button()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.TxtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.BtnShowPassword = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnLogin.Location = New System.Drawing.Point(300, 239)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(195, 38)
+        Me.BtnLogin.TabIndex = 0
+        Me.BtnLogin.Text = "Log In"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'BtnRegister
+        '
+        Me.BtnRegister.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnRegister.Location = New System.Drawing.Point(300, 280)
+        Me.BtnRegister.Name = "BtnRegister"
+        Me.BtnRegister.Size = New System.Drawing.Size(195, 38)
+        Me.BtnRegister.TabIndex = 1
+        Me.BtnRegister.Text = "Register"
+        Me.BtnRegister.UseVisualStyleBackColor = True
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtPassword.Location = New System.Drawing.Point(300, 205)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(195, 30)
+        Me.TxtPassword.TabIndex = 2
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtUsername.Location = New System.Drawing.Point(300, 141)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(195, 30)
+        Me.TxtUsername.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(352, 180)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 22)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Password"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(352, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 22)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Username"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 40.0!)
+        Me.Label3.Location = New System.Drawing.Point(280, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(229, 61)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Welcome"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 40.0!)
+        Me.Label4.Location = New System.Drawing.Point(263, 391)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(275, 61)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Cozy Hotel"
+        '
+        'BtnShowPassword
+        '
+        Me.BtnShowPassword.Font = New System.Drawing.Font("Times New Roman", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnShowPassword.Location = New System.Drawing.Point(501, 205)
+        Me.BtnShowPassword.Name = "BtnShowPassword"
+        Me.BtnShowPassword.Size = New System.Drawing.Size(37, 30)
+        Me.BtnShowPassword.TabIndex = 8
+        Me.BtnShowPassword.Text = "👁"
+        Me.BtnShowPassword.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(784, 461)
+        Me.Controls.Add(Me.BtnShowPassword)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.BtnRegister)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents BtnLogin As Button
+    Friend WithEvents BtnRegister As Button
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents TxtUsername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BtnShowPassword As Button
 End Class
